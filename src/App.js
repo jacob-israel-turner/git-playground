66749a015f4a9f18dfaf1d288068ce14c01762df
Jacob Turner <jacob.turner@domo.com>
2017-06-01 21:59:17 -0600

    Add operator

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import update from 'immutability-helper'
 import ab from 'autobind-decorator'
 
+const operators = ['+', '-', '*', '%', '/']
+
 @ab
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      values: ['', '']
+      values: ['', ''],
+      operatorIndex: 0,
     }
   }
 
@@ -20,11 +23,25 @@ class App extends Component {
     }))
   }
 
+  renderOperators(ops) {
+    return ops.map((op, index) => {
+      return <option value={index} key={index}>{op}</option>
+    })
+  }
+
+  handleOperatorChange({target: {value: index}}) {
+    this.setState({operatorIndex: Number(index)})
+  }
+
   render() {
     return (
       <div className="App">
         <input value={this.state.values[0]} onChange={this.updateValue.bind(this, 0)} />
         <input value={this.state.values[1]} onChange={this.updateValue.bind(this, 1)} />
+
+        <select value={this.state.operatorIndex} onChange={this.handleOperatorChange}>
+          {this.renderOperators(operators)}
+        </select>
       </div>
     );
   }
